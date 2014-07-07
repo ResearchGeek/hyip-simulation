a1be5cae3b88cc7b0c4e40afb901c06dccde3c7c
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-07-08 01:52:48 +0200

    formatting of HyipTypicalOffert.java

--- src/CredibilityGame/HyipTypicalOffert.java
+++ src/CredibilityGame/HyipTypicalOffert.java
@@ -2,15 +2,15 @@ package CredibilityGame;
 
 public class HyipTypicalOffert {
 
-	public static final HyipOffert LOW_RISK_1D = new HyipOffert(0.01, 5.0, true,
-			1, 20);
+	public static final HyipOffert LOW_RISK_1D = new HyipOffert(0.01, 5.0,
+			true, 1, 20);
 	public static final HyipOffert MEDIUM_RISK_1D = new HyipOffert(0.05, 50.0,
 			true, 1, 20);
 	public static final HyipOffert HIGH_RISK_1D = new HyipOffert(0.1, 1000.0,
 			true, 1, 20);
 
-	public static final HyipOffert LOW_RISK_7D = new HyipOffert(0.01, 5.0, true,
-			7, 50);
+	public static final HyipOffert LOW_RISK_7D = new HyipOffert(0.01, 5.0,
+			true, 7, 50);
 	public static final HyipOffert MEDIUM_RISK_7D = new HyipOffert(0.05, 50.0,
 			true, 7, 50);
 	public static final HyipOffert HIGH_RISK_7D = new HyipOffert(0.1, 1000.0,
